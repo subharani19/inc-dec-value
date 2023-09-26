396829f8c4ccdf8c3ff4423e4290f65c46503550
subharani19 <146064393+subharani19@users.noreply.github.com>
2023-09-26 15:24:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var RESULT=0;
+function decrase()
+{
+    RESULT=RESULT-5;
+    document.getElementById("count").textContent=RESULT;
+}
+function increase()
+{
+   RESULT=RESULT+5;
+    document.getElementById("count").textContent=RESULT;
+}
